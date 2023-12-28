8fa42596cd48d53662c6d680661c68acce696fc1
Farkhad17 <153022713+Farkhad17@users.noreply.github.com>
2023-12-28 12:49:00 +0400

    Update main.cpp

--- Finite-difference-methodFinite-difference-method/main.cpp
+++ Finite-difference-methodFinite-difference-method/main.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
+#include <gtest/gtest.h>
 
-#include <inv_matr.cpp>
-#include <progonka.cpp>
+#include "progonka.cpp"
+
+int n = 11;
+std::vector <double> u(n, 0);
+
+
+TEST(matrixTests,values)
+{
+	std::vector<double> testVec = {{2.0}, {2.02657}, {2.05253}, {2.07671}, {2.09741}, {2.11241}, {2.11906}, {2.11437}, {2.09516}, {2.05811}, {2.0}};
+	
+	for(int i = 0; i < 11; i++){
+		ASSERT_LT(abs(u[i] - testVec[i]), 2e-3);
+	}
+}
 
 int main(){
-	int n = 11, i;
+	int i;
 	double default_value = 0, h = 1.0/(n - 1);	
-	std::vector <double> u(n, default_value);
-	std::vector <double> u_n(n, default_value);
 	std::vector <double> u_nk(n, default_value);
 	
 	u_nk = progonka(n, h);
@@ -17,5 +28,9 @@ int main(){
 	for(i = 0; i < n; i++)std::cout << "u[" << i << "] = " << u_nk[i] << std::endl;
 	std::cout << std::endl; 
 	
-	return 0;
+	u = u_nk;
+	
+	::testing::InitGoogleTest();
+	
+	return RUN_ALL_TESTS();
 }
